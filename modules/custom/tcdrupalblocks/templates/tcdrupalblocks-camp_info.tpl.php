e62788a2e9378460ef72295fe4ff85d11173b5eb
Jeremiah Davis <jer@advantagelabs.com>
2015-02-25 21:48:20 -0600

    Updating dates in block

--- modules/custom/tcdrupalblocks/templates/tcdrupalblocks-camp_info.tpl.php
+++ modules/custom/tcdrupalblocks/templates/tcdrupalblocks-camp_info.tpl.php
@@ -1,8 +1,8 @@
 <ul>
-  <li><strong>Early-bird pricing ends</strong> - July 15th</li>
+  <li><strong>Early-bird pricing ends</strong> - May 15th</li>
   <li>Sessions announced - TBD</li>
-  <li>Training - August 7th</li>
-  <li>Camp - August 8th &amp; 9th</li>
-  <li>Sprints - August 7th-10th</li>
-  <li>Session submission closes - June 22nd</li>
+  <li>Training - June 25th</li>
+  <li>Camp - June 26th &amp; 27th</li>
+  <li>Sprints - June 25th-28th</li>
+  <li>Session submission closes - May 1st</li>
 </ul>
